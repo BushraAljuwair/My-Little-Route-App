c25a981503578668a8003d70454ea5b44310aba8
BushraAljuwair <bushraaljuwair@gmail.com>
2025-07-21 20:19:06 +0300

    remove google key

--- my_little_route/ios/Runner/AppDelegate.swift
+++ my_little_route/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyD5c9hagUGweCUuOA87v1UakWSoIrZQ7-4")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
