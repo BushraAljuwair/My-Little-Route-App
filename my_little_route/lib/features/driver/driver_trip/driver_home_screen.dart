31178c93098a58bce2409a903bc19d55eaeffbfd
BushraAljuwair <bushraaljuwair@gmail.com>
2025-08-03 12:18:20 +0300

    modifie driver trip

--- my_little_route/lib/features/driver/driver_trip/driver_home_screen.dart
+++ my_little_route/lib/features/driver/driver_trip/driver_home_screen.dart
@@ -4,11 +4,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_little_route/features/auth/login_screen.dart';
-import 'package:my_little_route/features/driver/driver_trip/bloc/driver_trip_bloc.dart';
+ import 'package:my_little_route/features/driver/driver_trip/bloc/driver_trip_bloc.dart';
 import 'package:my_little_route/features/driver/driver_trip/driver_trip_screen.dart';
 import 'package:my_little_route/features/driver/driver_trip/widgets/card/custom_card.dart';
-import 'package:my_little_route/repository/supabase.dart';
-import 'package:my_little_route/style/style_color.dart';
+ import 'package:my_little_route/style/style_color.dart';
 import 'package:my_little_route/style/style_size.dart';
 import 'package:my_little_route/style/style_text.dart';
 import 'package:my_little_route/utilities/extensions/screens/get_size_screen.dart';
@@ -47,12 +46,12 @@ class DriverHomeScreen extends StatelessWidget {
                         iconColor: StyleColor.buttonOrange,
                         tripTitle: "MorningTrip1",
                         onTap: () {
-                          if(bloc.sharedPrefs.getString("trip_id")==null) {
+                          // if(bloc.sharedPrefs.getString("trip_id")==null) {
                             log("1234567890-0987654321");
                             bloc.add(CreateTripEvent());
-                          }else{
-                            bloc.add(GetTripEvent());
-                          }
+                          // }else{
+                          //   bloc.add(GetTripEvent());
+                          // }
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -73,7 +72,18 @@ class DriverHomeScreen extends StatelessWidget {
                       CustomCard(
                         tripTitle: "ReturnTrip",
                         onTap: () {
-                          bloc.add(CreateReturnTripEvent());
+                          // bloc.add(CreateReturnTripEvent());
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return BlocProvider.value(
+                                  value: bloc,
+                                  child: LoginScreen(),
+                                );
+                              },
+                            ),
+                          );
                         },
                         icon: Icons.house,
                         iconColor: StyleColor.lapislazuli,
